f6ea13c5f409abc428cc9b095ebe5f7d2df38019
harsh tripathi <harshtripathih9t@gmail.com>
2023-05-09 18:27:49 +0530

    ip in front

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:8800/api/",
+  baseURL: "http://13.58.87.111:8800/api/",
   withCredentials: true,
 });
 
